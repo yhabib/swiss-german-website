7acb5d72f5ecb768084a3c4cfb1b1bca45d20b2b
Yusef Habib Fernandez <habibfernandez@gmail.com>
2026-03-18 08:03:58 +0100

    feat: analycits

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const nunito = Nunito({
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${nunito.variable} antialiased font-sans`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
